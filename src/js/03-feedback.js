df1273e27d30bb94d3b06aecf3e97c429d9fbf89
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2022-07-24 13:52:37 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,12 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('form');
+const formSubmitRef = document.querySelector('button');
 
 checkLocalStorageForm();
 
 formRef.addEventListener('input', throttle(onInputForm, 500, { leading: false }));
+formSubmitRef.addEventListener('click', onSubmitFormClick);
 
 function checkLocalStorageForm() {
   const formValue = JSON.parse(localStorage.getItem('feedback-form-state'));
@@ -37,5 +39,5 @@ function onSubmitFormClick(e) {
   console.log(formValue);
   formRef.reset();
   localStorage.removeItem('feedback-form-state');
-}
+};
 
