b77d5c3e4c8788fcbf9829bf3b907840047d0189
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2022-07-24 14:06:01 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,16 @@
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items.js';
 
 const listGallery = document.querySelector('.gallery');
 
+const gallery = new SimpleLightbox('.gallery__item', { captionSelector: 'img', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
 function createGallleryMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
+    const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
         return `<li class="gallery__card"><a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
       </a></li>`;
     }).join('');
+  listGallery.insertAdjacentHTML('beforeend', galleryMarkup);
 };
-
-listGallery.insertAdjacentHTML('beforeend', createGallleryMarkup(galleryItems));
-
-const gallery = new SimpleLightbox('.gallery__item', { captionSelector: 'img', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
